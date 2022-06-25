3ff22fabeaeb57ade6c0a006ba0276d37b50a250
Timothy McCarthy <ebh042@gmail.com>
2022-06-25 21:01:21 +1000

    Allow reading table/columns

--- core/src/main/scala/au/id/tmm/db/statements/DbSymbol.scala
+++ core/src/main/scala/au/id/tmm/db/statements/DbSymbol.scala
@@ -5,3 +5,11 @@ sealed trait DbSymbol
 final case class ColumnName(asString: String)                        extends DbSymbol
 final case class TableName(asString: String)                         extends DbSymbol
 final case class ColumnOnTable(table: TableName, column: ColumnName) extends DbSymbol
+
+object ColumnName {
+  implicit val fromResultSetColumn: FromResultSetColumn[ColumnName] = FromResultSetColumn[String].map(ColumnName.apply)
+}
+
+object TableName {
+  implicit val fromResultSetColumn: FromResultSetColumn[TableName] = FromResultSetColumn[String].map(TableName.apply)
+}
